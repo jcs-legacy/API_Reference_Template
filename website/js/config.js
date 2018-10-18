7ff3f8dc25666379c789cf65c6b93144e1704101
jenchieh <jcs090218@gmail.com>
2018-10-19 01:51:21 +0800

    Add search engine config.

--- website/js/config.js
+++ website/js/config.js
@@ -19,6 +19,8 @@ const copyright_text = "Copyright © [yyyy] [Group Name]. Built: [yyyy-mm-dd].";
 
 // Default content page.
 const intro_content = 'intro';
+// Search page.
+const search_content = 'search';
 
 // Content extension to load.
 //
@@ -27,3 +29,9 @@ const intro_content = 'intro';
 //
 // SEE : confid.js in the project root directory.
 const content_extension = '.html';
+
+// Search Input config
+const si_manual_placeholder = "Search manual...";
+const si_api_placeholder = "Search scripting...";
+
+const si_input_size = 17;  // character unit.
