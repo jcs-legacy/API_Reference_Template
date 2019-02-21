e424247d6edc26027add2b3c35f4a18bc367972c
jenchieh <jcs090218@gmail.com>
2019-02-21 15:54:39 +0800

    Update sorting.

--- main.js
+++ main.js
@@ -135,6 +135,9 @@ function search_api(req, res, next) {
 function getManualTree() {
   const tree = dirTree(config.MANUAL_DIR_PATH, { extensions: config.CONTENT_EXTENSION, normalizePath: true });
 
+  // Sort it.
+  sortTreeByType(tree.children);
+
   // Modefied the `MANUAL_DIR_PATH' to the correct format string.
   var removePath = config.MANUAL_DIR_PATH;
   removePath = removePath.replace("./", "");
@@ -147,6 +150,9 @@ function getManualTree() {
 /* Get the API tree. */
 function getAPITree() {
   const tree = dirTree(config.API_DIR_PATH, { extensions: config.CONTENT_EXTENSION, normalizePath: true });
+
+  // Sort it.
+  sortTreeByType(tree.children);
 
   // Modefied the `API_DIR_PATH' to the correct format string.
   var removePath = config.API_DIR_PATH;
@@ -204,6 +210,47 @@ function searchMatchPath(dir, match, arr) {
     // If match add it to search result.
     if (nameUpper.includes(matchUpper) && pathObj.type == 'file') {
       arr.push(pathObj);
+    }
+  }
+}
+
+/**
+ * Sort the tree result by directory/file type.
+ * @param { tree.children } tree : Tree children.
+ * @param { string } type : Sort type, enter 'directory' or 'file'.
+ */
+function sortTreeByType(tree, type = 'directory') {
+  let tarList = [];  // target list.
+  let anoList = [];  // another list.
+
+  /* Split path object into two arrays by type. */
+  {
+    for (let index = 0;
+         index < tree.length;
+         ++index)
+    {
+      let pathObj = tree[index];
+      if (pathObj.children != null && pathObj.children.length != 0) {
+        sortTreeByType(pathObj.children, type);
+      }
+
+      // Add path object by type.
+      if (pathObj.type == type)
+        tarList.push(pathObj);
+      else
+        anoList.push(pathObj);
+    }
+  }
+
+  /* Copy array over. */
+  {
+    let resultTree = tarList.concat(anoList);
+
+    for (let index = 0;
+         index < tree.length;
+         ++index)
+    {
+      tree[index] = resultTree[index];
     }
   }
 }
